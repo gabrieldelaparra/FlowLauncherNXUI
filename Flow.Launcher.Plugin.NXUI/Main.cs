0a514f9fd80d75f9eda65b97d609106cb3ba69ab
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2023-02-21 17:38:49 +0100

    Remove logging; Fix issue with calling another window in the Plugin;

--- Flow.Launcher.Plugin.NXUI/Main.cs
+++ Flow.Launcher.Plugin.NXUI/Main.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-
-using Flow.Launcher.Plugin;
 
 namespace Flow.Launcher.Plugin.NXUI
 {
@@ -17,36 +13,24 @@ namespace Flow.Launcher.Plugin.NXUI
 
         public NXUI()
         {
-            Log("Loading Plugin. Constructor");
             AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
         }
 
-        private const string LogPath = "C:\\Dev\\flowNXUI.log";
-
-        public void Log(string message)
-        {
-            //To avoid logging, uncomment next line.
-            File.AppendAllLines(LogPath, new[] { message });
-        }
 
         private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
             var fullName = args.Name;
-            Log($"CurrentDomain_AssemblyResolve:{args.Name}");
             var directoryInfo = new DirectoryInfo(_executionPath);
             var dlls = directoryInfo.EnumerateFiles("*.dll", SearchOption.TopDirectoryOnly);
             var assemblies = dlls.Select(x => Assembly.LoadFrom(x.FullName));
             var assembly = assemblies.FirstOrDefault(x => x.FullName.Equals(fullName));
-            Log($"Resolve:{assembly?.FullName ?? string.Empty}");
             return assembly;
         }
 
         public void Init(PluginInitContext context)
         {
-            Log("Loading Plugin. Init");
             if (context == null)
             {
-                Log("Exception: Null Context");
                 return;
             }
             _context = context;
@@ -54,12 +38,10 @@ namespace Flow.Launcher.Plugin.NXUI
             _executionPath = new FileInfo(_context.CurrentPluginMetadata.ExecuteFilePath).Directory?.FullName ?? string.Empty;
             if (string.IsNullOrWhiteSpace(_executionPath))
                 return;
-            Log($"ExecutionPath: {_executionPath}");
         }
 
         public List<Result> Query(Query query)
         {
-            Log("NXUI: Query");
             return new List<Result>()
             {
                 new Result()
@@ -67,16 +49,7 @@ namespace Flow.Launcher.Plugin.NXUI
                     Title = "Show NXUI",
                     Action = context =>
                     {
-                        try
-                        {
                             ShowDialog();
-                        }
-                        catch (Exception ex)
-                        {
-                            Log(ex.Message);
-                        }
-                        Log("Return");
-                        Log($"Count: {_count}");
                         return true;
                     }
                 }
@@ -92,30 +65,23 @@ namespace Flow.Launcher.Plugin.NXUI
                         .Children(
                             Button(out var button)
                                 .Content("Welcome to Avalonia, please click me!"),
-                            Button(out var button2)
-                                .Content("Welcome to Avalonia, please click me!"),
                             TextBox(out var tb1)
                                 .Text("NXUI"),
                             TextBox()
                                 .Text(window.BindTitle()),
                             Label()
                                 .Content(button.ObserveOnClick().Select(_ => ++_count)
-                                    .Select(x => $"You clicked {x} times.")),
-                            Label()
-                                .Content(button2.ObserveOnClick().Select(_ => ShowDialog()))
+                                    .Select(x => $"You clicked {x} times."))
                             ))
 
                 .Title(tb1.ObserveText().Select(x => x?.ToUpper()));
 
-            Log("Configure");
             _appBuilder = AppBuilder.Configure<Application>()
                 .UsePlatformDetect()
                 .UseFluentTheme()
                 .WithApplicationName("NXUI");
 
-            Log("Start");
             _appBuilder.StartWithClassicDesktopLifetime(Build, Array.Empty<string>());
-            Log("Closed");
         }
     }
 }
